971d2d0c31b87433542a82c88472447379a474e1
wicash <gianluca.wicash@gmail.com>
2021-03-25 21:46:03 +0100

    Controller edited

--- Wicash&Taxi_Platformer/src/Controller.java
+++ Wicash&Taxi_Platformer/src/Controller.java
@@ -17,7 +17,7 @@ public class Controller implements KeyListener{
 	@Override
 	public void keyTyped(KeyEvent e) {
 
-		// TODO Auto-generated method stubdasddasdasdadfdjgdfkgdgdkfgdfgTEST
+		// TODO Auto-generated method stubdasddasdasdadfdjgdfkgdgdkfgdfgTEST 12345
 	
 	}
 
